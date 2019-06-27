c7098dbb177dc1208b6298f134dabb0398fa67a8
Denys Horopashnyi <44978023+denyshoropashnyi@users.noreply.github.com>
2019-06-27 14:02:21 +0300

    - Change background in .panel__subhead--component--img to image. - Improve codestyle.

--- script/script.js
+++ script/script.js
@@ -13,3 +13,35 @@ viewUsedCarsBtn.addEventListener('click', goToUsedCarsPage);
 function goToUsedCarsPage() {
     window.location = 'https://www.infinitiretailgroup.co.uk/used-cars/';
 }
+
+let learnMoreQ30Btn = document.getElementById('learnMoreQ30Btn');
+learnMoreQ30Btn.addEventListener('click', goToQ30Page);
+
+function goToQ30Page() {
+    window.location = 'https://www.infinitiretailgroup.co.uk/used-cars/9724973-infiniti-q30-q30-1.5d-se-5-dr-business-pack-hatchback/';
+}
+
+
+let buyOnlineQ30Btn = document.getElementById('buyOnlineQ30Btn');
+buyOnlineQ30Btn.addEventListener('click', goToBuyOnlineQ30Page);
+
+function goToBuyOnlineQ30Page() {
+    window.location = 'https://www.infinitiretailgroup.co.uk/online-deposits/?vehicle=9724973&pre-selected-payment-method=purchase';
+}
+
+
+let learnMoreQ50Btn = document.getElementById('learnMoreQ50Btn');
+learnMoreQ50Btn.addEventListener('click', goToQ50Page);
+
+function goToQ50Page() {
+    window.location = 'https://www.infinitiretailgroup.co.uk/used-cars/10040992-infiniti-q50-q50-2.2d-premium-4-dr-auto-saloon/';
+}
+
+
+let buyOnlineQ50Btn = document.getElementById('buyOnlineQ50Btn');
+buyOnlineQ50Btn.addEventListener('click', goToBuyOnlineQ50Page);
+
+function goToBuyOnlineQ50Page() {
+    window.location = 'https://www.infinitiretailgroup.co.uk/online-deposits/?vehicle=10040992&pre-selected-payment-method=purchase';
+}
+

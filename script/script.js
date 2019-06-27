3c088cc69f8c41d047ac993fff050c6526a09081
Denys Horopashnyi <44978023+denyshoropashnyi@users.noreply.github.com>
2019-06-27 09:30:02 +0300

    Improve some code style after complete first panel.

--- script/script.js
+++ script/script.js
@@ -2,15 +2,14 @@
 
 let findDealerBtn = document.getElementById('findDealerBtn');
 findDealerBtn.addEventListener('click', goToFindDealerPage);
+
 function goToFindDealerPage() {
     window.location = 'https://www.infinitiretailgroup.co.uk/contact-us/';
 }
 
 let viewUsedCarsBtn = document.getElementById('viewUsedCarsBtn');
 viewUsedCarsBtn.addEventListener('click', goToUsedCarsPage);
+
 function goToUsedCarsPage() {
     window.location = 'https://www.infinitiretailgroup.co.uk/used-cars/';
 }
-
-
-
